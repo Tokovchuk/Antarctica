f4bd98863a5f0fc5b0f6f564df282da990a59b08
Tokovchuk <i.tokovchuk@gmail.com>
2020-09-18 00:22:38 +0300

    блок с круизами для просомтра наствнику

--- gulpfile.js
+++ gulpfile.js
@@ -67,9 +67,11 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
-    .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite_auto.svg"))
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
