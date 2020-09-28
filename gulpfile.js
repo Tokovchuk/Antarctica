4fcf1272744f9c052cf5bc10ea31860c8c3bd8b8
Tokovchuk <i.tokovchuk@gmail.com>
2020-09-28 13:56:15 +0300

    исправляет замечания второго теста

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task("images", function() {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
+      imagemin.jpegtran({quality: 75, progressive: true}),
       imagemin.svgo()
     ]))
 
